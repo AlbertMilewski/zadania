bc64d102282cd6e8aaf8587532e885ec440bc349
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-11-14 22:24:57 +0100

    Add files via upload

--- wielomian/wielomian/wielomian.cs
+++ wielomian/wielomian/wielomian.cs
@@ -9,51 +9,72 @@ namespace Horner_Consola
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine("Podaj stopień wielomioanu: ");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            int[] a = new int[n];
-            int[] b = new int[n];
-            double[] c = new double[n];
-            int o = n;
-            for (int i = 0; i < n; i++)
+            while (true)
             {
-                Console.WriteLine("Podaj współczynnik przy x^" + (o));
-                a[i] = Convert.ToInt32(Console.ReadLine());
-                o--;
+                try
+                {
+                    int n, x;
+                    Console.WriteLine("Podaj stopień wielomioanu: ");
+                    n = int.Parse(Console.ReadLine());
+
+                    int[] a = new int[n];
+                    int[] b = new int[n];
+                    double[] c = new double[n];
+                    int o = n;
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Podaj współczynnik przy x^" + (o));
+                        a[i] = int.Parse(Console.ReadLine());
+                        o--;
+                    }
+
+
+                    Console.WriteLine("Podaj x:");
+                    x = int.Parse(Console.ReadLine());
+                    int w;
+                    Console.WriteLine("Podaj wyraz wolny: ");
+                    w = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < n; i++)
+                    {
+                        //potęgowanie wyrazu z tablicy przez x
+                        b[i] = (a[i] * x);
+                    }
+                    int m = n;
+                    for (int i = 0; i < n; i++)
+                    {
+                        c[i] = (Math.Pow(b[i], m));
+                        m--;
+                    }
+
+
+                    double wynik = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        wynik += c[i];
+                    }
+
+
+                    Console.WriteLine("Wynik to: " + (wynik + w));
+                    Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wprowadzono liczbę w złym formacie");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Wprowadzona liczba jest poza dopuszczalnym zakresem");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Napotkano koniec strumienia");
+                }
+                Console.WriteLine("Spróbuj jeszcze raz");
             }
 
-            int x;
-
-            Console.WriteLine("Podaj x:");
-            x = Convert.ToInt32(Console.ReadLine());
-            int w;
-            Console.WriteLine("Podaj wyraz wolny: ");
-            w = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-                //mnożenie wyrazu przy x przez x
-                b[i] = (a[i] * x);
-            }
-            int m = n;
-            for (int i = 0; i < n; i++)
-            {
-                c[i] = (Math.Pow(b[i], m));
-                m--;
-            }
 
 
-            double wynik = 0;
-            for (int i = 0; i < n; i++)
-            {
-                wynik += c[i];
-            }
-         
-
-                Console.WriteLine("Wynik to: " + (wynik + w));
-                Console.ReadLine();
-        
         }
-    }
-}
+                }
+        }
+    
