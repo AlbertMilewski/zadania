9838881f900b4e8392b21033d267f9b814a38e6f
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-12-05 23:28:23 +0100

    Add files via upload

---
+++ histogram/histogram/histogram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace histogram
+{
+    class Program
+    {
+        static void gwaizdki(int n,int [] arr)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(j + ": ");
+
+                for (int i = 0; i < n; i++)
+                    if (arr[i] == j)
+                    {
+                        Console.Write("*");
+                    }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość ciągu");
+            
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                int[] arr = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+
+                    while (true)
+                    {
+                        Console.WriteLine("podaj liczbę z zakresu 1-5. Liczba:  " + (i + 1));
+                        int x = int.Parse(Console.ReadLine());
+                        if ((x >= 1) & (x <= 5))
+                        {
+
+
+                            arr[i] = x;
+                            break;
+                        }
+
+                        else if ((x <= 1) & (x >= 5))
+                        {
+                            Console.WriteLine("liczba nie z przedziału");
+                        }
+                        else
+                        {
+                            Console.WriteLine("To nie liczba");
+                        }
+                    }
+                } 
+                
+               
+                gwaizdki(n, arr);
+
+            }
+            Console.ReadLine();
+            }
+        }
+    }
+
