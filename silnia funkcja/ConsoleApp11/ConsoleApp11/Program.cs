e01b0f667f3edc9d850cee6d6a97cfcc6f053484
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-12-05 22:43:47 +0100

    Add files via upload

---
+++ silnia funkcja/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+       static int silnia(int n,int b)
+        {
+            
+            for (int i = 1; i < n; i++)
+            {
+                b += n * i;
+             }
+            return b;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program obliczania silni z danej liczby");
+            Console.WriteLine("Podaj liczbe");
+            int n = int.Parse(Console.ReadLine());
+            int b = 0;
+               b = silnia(n, b);
+         
+            Console.WriteLine("!"+n+" równa się "+ b);
+            Console.ReadLine();
+        }
+    }
+}
