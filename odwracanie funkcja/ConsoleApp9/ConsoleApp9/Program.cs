e1f5388a5b359f95dd6ba465803ec0c3ec18c147
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-12-05 22:44:25 +0100

    Add files via upload

---
+++ odwracanie funkcja/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void PrintArray(int[] array, int n)
+        {
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine("Podaj liczbę: "+(i+1));
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+        }
+        static void odwracanie(int [] array, int b)
+        {
+            for (int i=0; i<b; b--)
+            {
+                Console.WriteLine(array[b-1]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile liczb chcesz wprowadzić?");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            PrintArray(array, n);
+           
+            int b = n;
+            Console.WriteLine();
+            odwracanie(array, b);
+          
+            Console.ReadLine();
+        }
+    }
+}
