c16889d128054178b29b4f9e9fa17d9e6ef87876
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-12-05 22:33:14 +0100

    Add files via upload

---
+++ kwadrat bez przekątnej/ConsoleApp8/Funkcje.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static int Pytanie(ref int n)
+        {
+            n = int.Parse(Console.ReadLine());
+            return n;
+
+        }
+        static void obliczenia(ref int a,ref int n)
+        {
+            for (int b = 0; b < n; b++)
+            {
+                Console.WriteLine();
+                for (int i = 0; i < a - 1; i++)
+                {
+                    Console.Write("o");
+                }
+
+
+
+                Console.Write("x");
+                a--;
+                for (int i = 0; i < b; i++)
+                {
+                    Console.Write("o");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            int n,a,b;
+            Console.WriteLine("Podaj rozmiar kwadratu");
+            n = 0;
+            Pytanie(ref n);
+           
+            a = n;
+            obliczenia(ref a,ref n);
+            Console.ReadLine();
+        }
+    }
+}
