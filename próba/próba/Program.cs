fcda80aeae718d2456669adaa48c33926e663a4b
AlbertMilewski <33467542+AlbertMilewski@users.noreply.github.com>
2017-12-06 13:18:43 +0100

    Add files via upload

---
+++ próba/próba/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace próba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int A, B, N;
+                    string C, D;
+                    Console.WriteLine("Podaj wysokosc a=: ");
+                    A = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Podaj szerokosc b=: ");
+                    B = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Podaj pierwszy znak: ");
+                    C = Console.ReadLine();
+
+                    Console.WriteLine("Podaj drugi znak: ");
+                    D = Console.ReadLine();
+
+
+                    int wysokosc = A;
+                    int szerokosc = B;
+                    string znak1 = C;
+                    string znak2 = D;
+
+                    Console.WriteLine("");
+
+                    if ((wysokosc > 0) & (szerokosc > 0) & (znak1.Length == 1) & (znak2.Length == 1))
+                    {
+                        for (int i = 1; i <= A; i++)
+                        {
+
+                            for (int j = 1; j <= szerokosc; j++)
+                            {
+                                if (j % 2 == 0)
+                                {
+                                    Console.Write(znak1);
+
+                                }
+                                else if (j % 2 != 0)
+                                {
+                                    Console.Write(znak2);
+
+                                }
+
+                            }
+                            Console.WriteLine();
+
+                        }
+                        Console.ReadLine();
+
+                    }
+                    else if ((wysokosc < 0) | (szerokosc < 0))
+                    {
+                        Console.WriteLine("liczba ujemna");
+                        Console.ReadLine();
+
+                    }
+                    else if ((znak1.Length != 1) | (znak2.Length != 1))
+                    {
+                        Console.WriteLine("to nie znak");
+                        Console.ReadLine();
+                    }
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wprowadzono liczbę w złym formacie");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Wprowadzona liczba jest poza dopuszczalnym zakresem");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Napotkano koniec strumienia");
+                }
+                Console.WriteLine("Spróbuj jeszcze raz");
+            }
+        }
+
+    }
+
+    }
+
---
+++ próba/próba/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("próba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("próba")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("17d3fb02-fb32-427c-ab3e-4b701e8f897e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
